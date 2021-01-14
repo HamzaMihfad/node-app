bd2dd74a9a1785e9b8c47d36ae029a5b88d7591c
Hamza Mihfad <mihfad1hamza@gmail.com>
2021-01-14 18:16:23 +0100

    lodash

--- server.js
+++ server.js
@@ -1,8 +1,20 @@
 const http = require('http');
 const fs = require('fs');
+const _ = require('lodash');
 
 const server = http.createServer((req, res)=>{
     //console.log(req.url, req.method);
+
+
+    // lodash
+    let num = _.random(0,100);
+    console.log(num);
+
+    const greet = _.once(() => {
+        console.log('hello');
+      });
+      greet();
+      greet();
 
     //set header content type for the response:
     res.setHeader('Content-Type', 'text/html');
