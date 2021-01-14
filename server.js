c89fce2e9367b1f75ced99ba1c699a56dc9c5959
Hamza Mihfad <mihfad1hamza@gmail.com>
2021-01-14 11:53:08 +0100

    port 5000

--- server.js
+++ server.js
@@ -53,5 +53,5 @@ const server = http.createServer((req, res)=>{
 })
 
 server.listen(3000, 'localhost', ()=>{
-    console.log('listening for requests on port 3000');
+    console.log('listening for requests on port 5000!');
 });
